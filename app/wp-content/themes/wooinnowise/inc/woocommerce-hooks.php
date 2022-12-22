d31a146f84b539ab230b7e21ee2db24430f1123e
Rc561U <hugeanton29011994@gmail.com>
2022-12-22 17:34:42 +0300

    add rating, owl, content-product

--- app/wp-content/themes/wooinnowise/inc/woocommerce-hooks.php
+++ app/wp-content/themes/wooinnowise/inc/woocommerce-hooks.php
@@ -2,3 +2,25 @@
 
 add_filter( 'woocommerce_enqueue_styles', '__return_false' );
 
+remove_action( 'woocommerce_shop_loop_subcategory_title', 'woocommerce_template_loop_category_title', 10 );
+add_action( 'woocommerce_shop_loop_subcategory_title', function ( $category ) {
+    echo '<h6>' . esc_html( $category->name ) . '</h6>';
+    echo '<small class="text-body">' . $category->count . __( ' Products', 'woostudy' ) . '</small>';
+} );
+
+remove_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10 );
+remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5 );;
+
+
+
+//remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10 );;
+
+
+remove_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10 );
+add_action( 'woocommerce_shop_loop_item_title', function () {
+    // https://woocommerce.github.io/code-reference/classes/WC-Product.html
+    global $product;
+    /** @var WC_Product $product */
+    echo '<a class="h6 text-decoration-none text-truncate" href="' . $product->get_permalink() . '">' . $product->get_title() . '</a>';
+} );
+remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5 );
