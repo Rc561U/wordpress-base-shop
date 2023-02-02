87f0799e6729e8dadec4415f99c0976e8e23a90f
Rc561U <hugeanton29011994@gmail.com>
2023-02-02 18:04:12 +0300

    feature: add localstack service

--- app/wp-content/themes/wooinnowise/functions.php
+++ app/wp-content/themes/wooinnowise/functions.php
@@ -1,35 +1,37 @@
 <?php
 
-function woostudy_setup() {
-	load_theme_textdomain( 'woostudy', get_template_directory() . '/languages' );
-	add_theme_support( 'title-tag' );
-	add_theme_support( 'post-thumbnail' );
-	add_theme_support( 'woocommerce' );
-	add_theme_support( 'wc-product-gallery-zoom' );
-	add_theme_support( 'wc-product-gallery-lightbox' );
-	add_theme_support( 'wc-product-gallery-slider' );
+function woostudy_setup()
+{
+    load_theme_textdomain('woostudy', get_template_directory() . '/languages');
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnail');
+    add_theme_support('woocommerce');
+    add_theme_support('wc-product-gallery-zoom');
+    add_theme_support('wc-product-gallery-lightbox');
+    add_theme_support('wc-product-gallery-slider');
 
-	register_nav_menus(
-		array(
-			'menu-1' => __( 'Top Menu', 'woostudy' ),
-			'menu-2' => __( 'Categories Menu', 'woostudy' ),
-			'menu-3' => __( 'Navbar Menu', 'woostudy' ),
-		)
-	);
+    register_nav_menus(
+        array(
+            'menu-1' => __('Top Menu', 'woostudy'),
+            'menu-2' => __('Categories Menu', 'woostudy'),
+            'menu-3' => __('Navbar Menu', 'woostudy'),
+        )
+    );
 }
 
-add_action( 'after_setup_theme', 'woostudy_setup' );
+add_action('after_setup_theme', 'woostudy_setup');
 
-function woostudy_widgets_init() {
-	register_sidebar(
-		array(
-			'name' => esc_html__( 'Sidebar', 'woostudy' ),
-			'id' => 'sidebar-1',
-			'description' => esc_html__( 'Add widgets here.', 'woostudy' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget' => '</section>',
-		)
-	);
+function woostudy_widgets_init()
+{
+    register_sidebar(
+        array(
+            'name' => esc_html__('Sidebar', 'woostudy'),
+            'id' => 'sidebar-1',
+            'description' => esc_html__('Add widgets here.', 'woostudy'),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget' => '</section>',
+        )
+    );
 }
 add_action( 'widgets_init', 'woostudy_widgets_init' );
 
@@ -44,13 +46,14 @@ function woostudy_scripts() {
 	wp_enqueue_style( 'woostudy-main', get_template_directory_uri() . '/assets/css/style.css' );
 	wp_enqueue_style( 'woostudy-custom', get_template_directory_uri() . '/assets/css/custom.css' );
 
-	wp_enqueue_script( 'jquery' );
-	wp_enqueue_script( 'woostudy-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js', array(), false, true );
-	wp_enqueue_script( 'woostudy-easing', get_template_directory_uri() . '/assets/lib/easing/easing.min.js', array(), false, true );
-	wp_enqueue_script( 'woostudy-owlcarousel', get_template_directory_uri() . '/assets/lib/owlcarousel/owl.carousel.min.js', array(), false, true );
-	wp_enqueue_script( 'woostudy-main', get_template_directory_uri() . '/assets/js/main.js', array(), false, true );
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('woostudy-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js', array(), false, true);
+    wp_enqueue_script('woostudy-easing', get_template_directory_uri() . '/assets/lib/easing/easing.min.js', array(), false, true);
+    wp_enqueue_script('woostudy-owlcarousel', get_template_directory_uri() . '/assets/lib/owlcarousel/owl.carousel.min.js', array(), false, true);
+    wp_enqueue_script('woostudy-main', get_template_directory_uri() . '/assets/js/main.js', array(), false, true);
 }
-add_action( 'wp_enqueue_scripts', 'woostudy_scripts' );
+
+add_action('wp_enqueue_scripts', 'woostudy_scripts');
 
 
 require_once get_template_directory() . '/inc/woocommerce-hooks.php';
@@ -58,9 +61,10 @@ require_once get_template_directory() . '/inc/class-woostudy-menu-categories.php
 require_once get_template_directory() . '/inc/class-woostudy-menu-navbar.php';
 require_once get_template_directory() . '/inc/cpt.php';
 
-function woostudy_debug( $data, $die = false ) {
-	echo '<pre>' . print_r( $data, 1 ) . '</pre>';
-	if ( $die ) {
-		die;
-	}
+function woostudy_debug($data, $die = false)
+{
+    echo '<pre>' . print_r($data, 1) . '</pre>';
+    if ($die) {
+        die;
+    }
 }
